0ba05b3ef22bfa4da0f73e48767c6004da222ad7
Deepak Koirala <dksofts@gmail.com>
2020-01-03 22:31:22 -0600

    adddelopt

--- js/main.js
+++ js/main.js
@@ -5,4 +5,22 @@ window.onload = () => {
     navigator.serviceWorker
              .register('./sw.js');
   }
+  
+  document.getElementById('att3').addEventListener('click',function(){
+	caches.keys().then(function(cacheNames) {
+		console.log(cacheNames);
+      return Promise.all(
+        cacheNames.filter(function(cacheName) {
+          // Return true if you want to remove this cache,
+          // but remember that caches are shared across
+          // the whole origin
+		  return true;
+        }).map(function(cacheName) {
+			console.log(cacheName);
+          return caches.delete(cacheName);
+        })
+      );
+    })
+});
 }
+
